55df1afa8dd2bb082bdadd9faf278f771816742b
ellesiba <rochelle.sibaja@gamil.com>
2023-02-03 21:06:46 -0500

    Buttons take and add health, but don't disappear

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //========== Enter Game ==============
-function GameStart(){
+function gameStart(){
   document.getElementById("start-button").style.display = "none";
   document.getElementById("guideBtn").style.display = "none"
   document.getElementById("title").style.display = "none"
@@ -22,205 +22,85 @@ toggle.addEventListener('change', function() {
   }
 });
 
-
 //========== Instructions ==============
+function instructions(){
+  alert("In this two player game you can play as Heartthrob or Heartbreaker to battle one another to see who truly is the best.");
+  alert("You can lower the other player's hp by singing or do increased damage by dancing");
+  alert("If you are hurt, you can use you healing visuals to perk yourself up.")
+  alert("May the best idol win!")
+}
+
+//================================================================================
+
+      const MAX_HEALTH = 100;
+
+      class Player {
+        constructor(name) {
+          this.name = name;
+          this.health = MAX_HEALTH;
+        }
+
+        attack(opponent) {
+          opponent.health -= 10;
+        }
+
+        heal() {
+          this.health = Math.min(this.health + 10, MAX_HEALTH);
+        }
+
+        block() {
+          this.health += 5;
+        }
+      }
+
+      const player1 = new Player('Player 1');
+      const player2 = new Player('Player 2');
+
+      let currentPlayer = player1;
+
+      const healthDisplay1 = document.getElementById('player1-health');
+      const healthDisplay2 = document.getElementById('player2-health');
+      const turnDisplay = document.getElementById('turn');
+
+function updateDisplay() {
+  healthDisplay1.style.width = `${(player1.health / MAX_HEALTH) * 100}%`;
+  healthDisplay2.style.width = `${(player2.health / MAX_HEALTH) * 100}%`;
+  turnDisplay.innerText = `${currentPlayer.name}'s turn`;
+
+  const attackButtonP1 = document.getElementById("p1-attack");
+  const blockButtonP1 = document.getElementById("p1-block");
+  const healButtonP1 = document.getElementById("p1-heal");
+
+  if (currentPlayer === player1) {
+    attackButtonP1.style.display = "inline";
+    blockButtonP1.style.display = "inline";
+    healButtonP1.style.display = "inline";
+  } else {
+    attackButtonP1.style.display = "none";
+    blockButtonP1.style.display = "none";
+    healButtonP1.style.display = "none";
+  }
+}
 
 
+      updateDisplay();
 
+      function handleAttack() {
+        currentPlayer.attack(currentPlayer === player1 ? player2 : player1);
+        switchTurn();
+      }
 
+      function handleHeal() {
+        currentPlayer.heal();
+        switchTurn();
+      }
 
+      function handleBlock() {
+        currentPlayer.block();
+        switchTurn();
+      }
 
-// let currentPlayer = 1;
-// let player1Health = 100;
-// let player2Health = 100;
-
-// const attack = () => {
-//   if (currentPlayer === 1) {
-//     player2Health -= 10;
-//     document.querySelector("#player2Health").innerHTML = player2Health;
-//   } else {
-//     player1Health -= 10;
-//     document.querySelector("#player1Health").innerHTML = player1Health;
-//   }
-//   checkStatus();
-// };
-
-// const block = () => {
-//   if (currentPlayer === 1) {
-//     player1Health += 5;
-//     document.querySelector("#player1Health").innerHTML = player1Health;
-//   } else {
-//     player2Health += 5;
-//     document.querySelector("#player2Health").innerHTML = player2Health;
-//   }
-// };
-
-// const heal = () => {
-//   if (currentPlayer === 1) {
-//     player1Health += 10;
-//     document.querySelector("#player1Health").innerHTML = player1Health;
-//   } else {
-//     player2Health += 10;
-//     document.querySelector("#player2Health").innerHTML = player2Health;
-//   }
-// };
-
-// const checkStatus = () => {
-//   if (player1Health <= 0) {
-//     document.querySelector("#status").innerHTML = "Player 2 wins!";
-//     document.querySelector("#attack").setAttribute("disabled", true);
-//     document.querySelector("#block").setAttribute("disabled", true);
-//     document.querySelector("#heal").setAttribute("disabled", true);
-//   } else if (player2Health <= 0) {
-//     document.querySelector("#status").innerHTML = "Player 1 wins!";
-//     document.querySelector("#attack").setAttribute("disabled", true);
-//     document.querySelector("#block").setAttribute("disabled", true);
-//   }
-// }
-
-// //----------------Creating Player Classes-----------------------
-// class Player  {
-//   constructor(dance, sing, visual, visualth){
-//     this.dance = dance
-//     this.sing = sing
-//     this.visual = visual
-//     this.health = 100;
-//   }
-// };
-
-// player1 = Player()
-// player2 = Player()
-
-// //----------------Move Buttons/Event Listeners-----------------------
-// // Add event listeners to the move buttons
-// const moveButtons = document.querySelectorAll("#move-button");
-// for (let i = 0; i < moveButtons.length; i++) {
-//   moveButtons[i].addEventListener("click", function() {
-//     const move = playerPokemon.moves[i];
-//     opponentPokemon.health -= move.damage;
-
-//     // Check if player is fainted
-//     if (opponentPokemon.health <= 0) {
-//       alert("Opponent Pokemon fainted!");
-//     } else {
-//       alert("Opponent Pokemon took " + move.damage + " damage!");
-//     }
-//   });
-// }
-
-// //----------------Health Bar-----------------------
-
-// //Player 1--------------------
-
-
-
-// //-----------------------------------------------------------------------------------------
-
-// // Get references to the controls for each player
-// // let player1Controls = document.getElementById("player1-controls");
-// // var player2Controls = document.getElementById("player2-controls");
-
-// // Initialize a variable to keep track of whose turn it is
-
-// // Function to switch the current player
-// function switchPlayer() {
-//   currentPlayer = (currentPlayer === 1) ? 2 : 1;
-// }
-
-// // Function to hide the controls for one player and show the controls for the other player
-// function updateControlsVisibility() {
-//   if (currentPlayer === 1) {
-//     player1Controls.style.display = "flex";
-//     player2Controls.style.display = "none";
-//   } else {
-//     player1Controls.style.display = "none";
-//     player2Controls.style.display = "flex";
-//   }
-// }
-
-// // Call the updateControlsVisibility function initially to hide the controls for player 2
-// updateControlsVisibility();
-
-// // Get references to the controls for each player
-// var player1Controls = document.getElementById("player1-controls");
-// var player2Controls = document.getElementById("player2-controls");
-
-// // Get references to the health bars for each player
-// var player1HealthBar = document
-
-// //------------------------------------------------------------------------------------------
-
-// let currentPlayer = 1;
-// // let player1Health = 100;
-// // let player2Health = 100;
-
-// //---------------Attack-------------------------------------
-
-// const vocal = () => {
-//   if (currentPlayer === 1) {
-//     player2Health -= 10;
-//     document.querySelector("#health-level").style.width = player2.health + "%"
-//   } else {
-//     player1Health -= 10;
-//     document.querySelector("#health-level-p2").style.width = player1.health;
-//   }
-//   checkStatus();
-//   switchTurn();
-// };
-
-
-// //---------------Block-------------------------------------
-
-// const dance = () => {
-//   if (currentPlayer === 1) {
-//     player1Health += 5;
-//     document.querySelector("#player1Health").innerHTML = player1Health;
-//   } else {
-//     player2Health += 5;
-//     document.querySelector("#player2Health").innerHTML = player2Health;
-//   }
-//   switchTurn();
-// };
-
-// //---------------Heal-------------------------------------
-// const visual = () => {
-//   if (currentPlayer === 1) {
-//     player1Health += 20;
-//     document.getElementById("#player1Health").innerHTML = player1Health;
-//   } else {
-//     player2Health += 20;
-//     document.querySelector("#player2Health").innerHTML = player2Health;
-//   }
-//   switchTurn();
-// };
-
-// //----------------Check Health---------------------------------
-
-// const checkStatus = () => {
-//   if (player1Health <= 0) {
-//     document.querySelector("#status").innerHTML = "Player 2 wins!";
-//     document.querySelector("#vocal").setAttribute("disabled", true);
-//     document.querySelector("#dance").setAttribute("disabled", true);
-//     document.querySelector("#visual").setAttribute("disabled", true);
-//   } else if (player2Health <= 0) {
-//     document.querySelector("#status").innerHTML = "Player 1 wins!";
-//     document.querySelector("#vocal").setAttribute("disabled", true);
-//     document.querySelector("#dance").setAttribute("disabled", true);
-//     document.querySelector("#visual").setAttribute("disabled", true);
-//   }
-// };
-
-// //----------------Switch---------------------------------------------
-// const switchTurn = () => {
-//   if (currentPlayer === 1) {
-//     currentPlayer = 2;
-//     document.querySelector("#currentPlayer").innerHTML = "Player 2's turn";
-//   } else {
-//     currentPlayer = 1;
-//     document.querySelector("#currentPlayer").innerHTML = "Player 1's turn";
-//   }
-// };
-
-// document.querySelector("#vocal").addEventListener("click", vocal);
-// document.querySelector("#dance").addEventListener("click", dance);
-// document.querySelector("#visual").addEventListener("click", visual);
+      function switchTurn() {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        updateDisplay();
+      }
